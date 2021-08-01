382e4081c7a50a08c6866d70bb8acb0ddd754a52
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-08-01 20:55:55 +0900

    Move and rename directory-listing.json to ../dir-list.json

--- .dir-list/dist/index.js
+++ .dir-list/dist/index.js
@@ -6,27 +6,29 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
-var baseDir = '..';
+var baseDir = "..";
 function buildPath() {
     var dirList = [];
     for (var _i = 0; _i < arguments.length; _i++) {
         dirList[_i] = arguments[_i];
     }
-    return __spreadArray([baseDir], dirList).join('/');
+    return __spreadArray([baseDir], dirList).join("/");
 }
 function isDir(path) {
     var stats = fs_1.statSync(path);
     return stats.isDirectory();
 }
 function isDotfile(name) {
-    return name.startsWith('.');
+    return name.startsWith(".");
 }
 var seriesList = fs_1.readdirSync(buildPath())
     .filter(function (series) { return !isDotfile(series) && isDir(buildPath(series)); })
     .map(function (series) {
-    var children = fs_1.readdirSync(buildPath(series)).filter(function (spec) {
+    var children = fs_1.readdirSync(buildPath(series))
+        .filter(function (spec) {
         return !isDotfile(spec) && isDir(buildPath(series, spec));
-    }).map(function (spec) {
+    })
+        .map(function (spec) {
         var children = fs_1.readdirSync(buildPath(series, spec)).filter(function (version) {
             return !isDotfile(spec);
         });
@@ -34,4 +36,4 @@ var seriesList = fs_1.readdirSync(buildPath())
     });
     return { name: series, children: children };
 });
-fs_1.writeFileSync('directory-listing.json', JSON.stringify(seriesList, null, 2));
+fs_1.writeFileSync("../dir-list.json", JSON.stringify(seriesList, null, 2));
