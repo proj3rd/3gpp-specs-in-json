869e56eae728704541156763654c88c1975bb02a
Seokseong Jeon <seokseong@jeon.engineer>
2023-03-08 00:48:45 +0900

    Fix wrong import of assert

--- .test/dist/xnap.js
+++ .test/dist/xnap.js
@@ -1,7 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.tcListXnap = void 0;
-var console_1 = require("console");
+var assert_1 = __importDefault(require("assert"));
 var definitions_1 = require("lib3rd/dist/ran3/classes/definitions");
 exports.tcListXnap = [
     {
@@ -14,14 +17,14 @@ exports.tcListXnap = [
             }
             it("SN UL PDCP UP TNL Information of PDU Session Resource Setup Response Info - SN terminated should refer 9.2.3.76", function () {
                 var defFound = def.findDefinition("9.2.1.6");
-                (0, console_1.assert)(defFound);
+                (0, assert_1.default)(defFound);
                 var elemFound = defFound === null || defFound === void 0 ? void 0 : defFound.elementList.find(function (elem) {
                     var name = elem.name;
                     return name === "SN UL PDCP UP TNL Information";
                 });
-                (0, console_1.assert)(elemFound);
+                (0, assert_1.default)(elemFound);
                 var tokenList = elemFound === null || elemFound === void 0 ? void 0 : elemFound.reference.trim().split(" ");
-                (0, console_1.assert)(tokenList === null || tokenList === void 0 ? void 0 : tokenList.includes("9.2.3.76"));
+                (0, assert_1.default)(tokenList === null || tokenList === void 0 ? void 0 : tokenList.includes("9.2.3.76"));
             });
         },
     },
@@ -35,7 +38,7 @@ exports.tcListXnap = [
             }
             it("9.1.1.8 RETRIEVE UE CONTEXT REQUEST should exist", function () {
                 var defFound = def.findDefinition("9.1.1.8");
-                (0, console_1.assert)(defFound);
+                (0, assert_1.default)(defFound);
             });
         },
     },
@@ -49,7 +52,7 @@ exports.tcListXnap = [
             }
             it("9.1.1.9 RETRIEVE UE CONTEXT RESPONSE should exist", function () {
                 var defFound = def.findDefinition("9.1.1.9");
-                (0, console_1.assert)(defFound);
+                (0, assert_1.default)(defFound);
             });
         },
     },
@@ -63,7 +66,7 @@ exports.tcListXnap = [
             }
             it("9.1.1.10 RETRIEVE UE CONTEXT FAILURE should exist", function () {
                 var defFound = def.findDefinition("9.1.1.10");
-                (0, console_1.assert)(defFound);
+                (0, assert_1.default)(defFound);
             });
         },
     },
@@ -77,7 +80,7 @@ exports.tcListXnap = [
             }
             it("9.1.1.12 RETRIEVE UE CONTEXT CONFIRM should exist", function () {
                 var defFound = def.findDefinition("9.1.1.12");
-                (0, console_1.assert)(defFound);
+                (0, assert_1.default)(defFound);
             });
         },
     },
