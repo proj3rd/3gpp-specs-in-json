7591784a90d753633c99b50a5b0511ec8cdb4948
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2024-05-15 14:49:49 +0900

    Move dir list builder

---
+++ .internal/build-dir-list.ts
@@ -0,0 +1,31 @@
+import { readdirSync, statSync, writeFileSync } from "fs";
+import { resolve } from "path";
+
+const BASE_DIR = "..";
+
+function path(...dirList: string[]): string {
+  return resolve(BASE_DIR, ...dirList);
+}
+
+function isSpecDir(...paths: string[]) {
+  const last = paths[paths.length - 1];
+  if (last.startsWith(".")) {
+    return false;
+  }
+  const stats = statSync(path(...paths));
+  return stats.isDirectory();
+}
+
+const seriesList = readdirSync(path())
+  .filter((series) => isSpecDir(series))
+  .map((series) => {
+    const children = readdirSync(path(series))
+      .filter((spec) => isSpecDir(series, spec))
+      .map((spec) => {
+        const children = readdirSync(path(series, spec));
+        return { name: spec, children };
+      });
+    return { name: series, children };
+  });
+
+writeFileSync(path(".dir-list.json"), JSON.stringify(seriesList, null, 2));
