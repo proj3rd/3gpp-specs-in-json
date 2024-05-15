4713e8552f059b1c461294d6d123ed8aafd36af4
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2024-05-15 18:09:21 +0900

    Integrate RAN3 tabular serialize script

--- .internal/.updater/batch-serialize-ran3-tabular.sh
+++
@@ -1,5 +0,0 @@
-FILES="./*.docx"
-for file in $FILES
-do
-	npx proj3rd/serialize-ran3-tabular $file 2>$file.log
-done
--- .internal/serialize.ps1
+++ .internal/serialize.ps1
@@ -3,21 +3,24 @@ Foreach ($line in $lines) {
     Write-Output $line
     # [spec, rel, date, "asn1"|"tabular"|"both"]
     $tokens = $line.Split(" ")
+    $base = "$($tokens[0])-$($tokens[1])-$($tokens[2])"
     # Download the spec
     npx -y get-3gpp-spec $tokens[0] $tokens[1] $tokens[2]
     # Unzip the spec
     Expand-Archive *.zip -DestinationPath .
     # Convert to plain text
     $docx = Get-ChildItem *.docx
-    $txt = "$($tokens[0])-$($tokens[1])-$($tokens[2]).txt"
+    $txt = "$base.txt"
     pandoc -f docx -t plain --wrap=none $docx -o $txt
     # Extract ASN.1
-    $asn1 = "$($tokens[0])-$($tokens[1])-$($tokens[2]).asn1"
+    $asn1 = "$base.asn1"
     npx -y extract-asn1 --exclude-non-tag-comment $txt > $asn1
     if ($tokens[3] -eq "asn1" -or $tokens[3] -eq "both") {
         # TODO: Serialize ASN.1
     }
+    # Serialize RAN3 tabular
+    $tabular_log = "$base.tabular.log"
     if ($tokens[3] -eq "tabular" -or $tokens[3] -eq "both") {
-        # TODO: Serialize Tabular
+        npx proj3rd/serialize-ran3-tabular $docx 2>$tabular_log
     }
 }
